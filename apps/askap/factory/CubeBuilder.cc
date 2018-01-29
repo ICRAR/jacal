edfa11f128f2f394b412dba67240ef43b974a552
Stephen Ord <steve.ord@gmail.com>
2018-01-29 15:43:01 +1100

    Added a simple cubebuilder that builds an empty cube from the contents of the parset

--- apps/askap/factory/CubeBuilder.cc
+++ apps/askap/factory/CubeBuilder.cc
@@ -51,6 +51,9 @@ namespace askap {
 #include <dataaccess/IDataIterator.h>
 #include <dataaccess/SharedIter.h>
 
+// Image accessors
+#include <imageaccess/ImageAccessFactory.h>
+
 // params helpers
 #include <measurementequation/SynthesisParamsHelper.h>
 #include <measurementequation/ImageParamsHelper.h>
@@ -62,6 +65,20 @@ namespace askap {
 #include <gridding/VisGridderFactory.h>
 
 
+#include <utils/PolConverter.h>
+
+// casacore
+
+#include <casacore/casa/Arrays/IPosition.h>
+#include <casacore/coordinates/Coordinates/SpectralCoordinate.h>
+#include <casacore/coordinates/Coordinates/DirectionCoordinate.h>
+#include <casacore/coordinates/Coordinates/StokesCoordinate.h>
+#include <casacore/coordinates/Coordinates/CoordinateSystem.h>
+#include <casacore/measures/Measures/Stokes.h>
+#include <casacore/images/Images/PagedImage.h>
+#include <casacore/casa/Quanta/Unit.h>
+#include <casacore/casa/Quanta/QC.h>
+
 #include <fitting/Params.h>
 
 
@@ -70,6 +87,7 @@ namespace askap {
 
 
 
+ASKAP_LOGGER(logger, ".CubeBuilder");
 
 namespace askap {
 
@@ -77,6 +95,133 @@ namespace askap {
         DaliugeApplication(raw_app)
     {
 
+    }
+
+      boost::shared_ptr<accessors::IImageAccess> CubeBuilder::Construct(const std::string& name) {
+        // as long as the cube exists all should be fine
+        ASKAPLOG_INFO_STR(logger, "Instantiating Cube Builder with existing cube ");
+        boost::shared_ptr<accessors::IImageAccess> itsCube = accessors::imageAccessFactory(this->itsParset);
+
+        vector<string> filenames;
+        /// Image name from parset - must start with "image."
+
+        if (itsParset.isDefined("Images.Names")) {
+            filenames = itsParset.getStringVector("Images.Names", true);
+            itsFilename = filenames[0];
+        }
+        else if(itsParset.isDefined("Images.name")) {
+            itsFilename = itsParset.getString("Images.name");
+        }
+        else {
+            ASKAPLOG_ERROR_STR(logger, "Could not find the image name(s) ");
+        }
+        ASKAPCHECK(itsFilename.substr(0,5)=="image",
+                   "Images.name (Names) must start with 'image' starts with " << itsFilename.substr(0,5));
+
+        // If necessary, replace "image" with _name_ (e.g. "psf", "weights")
+        // unless name='restored', in which case we append ".restored"
+        if (!name.empty()) {
+            if (name == "restored") {
+                itsFilename = itsFilename + ".restored";
+            } else {
+                const string orig = "image";
+                const size_t f = itsFilename.find(orig);
+                itsFilename.replace(f, orig.length(), name);
+            }
+        }
+
+        ASKAPLOG_INFO_STR(logger, "Contructing cube instance using existing file " << itsFilename);
+
+        return itsCube;
+    }
+    boost::shared_ptr<accessors::IImageAccess>  CubeBuilder::Construct(
+                             const casa::uInt nchan,
+                             const casa::Quantity& f0,
+                             const casa::Quantity& inc,
+                             const std::string& name)
+    {
+        ASKAPLOG_INFO_STR(logger, "Instantiating Cube by creating cube file");
+          boost::shared_ptr<accessors::IImageAccess> itsCube = accessors::imageAccessFactory(this->itsParset);
+
+        vector<std::string> filenames;
+
+
+        if (itsParset.isDefined("Images.Names")) {
+            filenames = itsParset.getStringVector("Images.Names", true);
+            itsFilename = filenames[0];
+        }
+        else if(itsParset.isDefined("Images.name")) {
+            itsFilename = itsParset.getString("Images.name");
+        }
+        else {
+            ASKAPLOG_ERROR_STR(logger, "Could not find the image name(s) ");
+        }
+        ASKAPCHECK(itsFilename.substr(0,5)=="image",
+                   "Images.name (Names) must start with 'image' starts with " << itsFilename.substr(0,5));
+
+        // If necessary, replace "image" with _name_ (e.g. "psf", "weights")
+        // unless name='restored', in which case we append ".restored"
+        if (!name.empty()) {
+            if (name == "restored") {
+                itsFilename = itsFilename + ".restored";
+            } else {
+                const string orig = "image";
+                const size_t f = itsFilename.find(orig);
+                itsFilename.replace(f, orig.length(), name);
+            }
+        }
+
+        const std::string restFreqString = itsParset.getString("Images.restFrequency", "-1.");
+        if (restFreqString == "HI") {
+            itsRestFrequency = casa::QC::HI;
+        } else {
+            itsRestFrequency = synthesis::SynthesisParamsHelper::convertQuantity(restFreqString, "Hz");
+        }
+
+
+        // Polarisation
+        const std::vector<std::string>
+            stokesVec = itsParset.getStringVector("Images.polarisation", std::vector<std::string>(1,"I"));
+        // there could be many ways to define stokes, e.g. ["XX YY"] or ["XX","YY"] or "XX,YY"
+        // to allow some flexibility we have to concatenate all elements first and then
+        // allow the parser from PolConverter to take care of extracting the products.
+        std::string stokesStr;
+        for (size_t i=0; i<stokesVec.size(); ++i) {
+            stokesStr += stokesVec[i];
+        }  /// Description of the polarisation properties of the output cubes
+
+
+        itsStokes = scimath::PolConverter::fromString(stokesStr);
+        const casa::uInt npol=itsStokes.size();
+
+        // Get the image shape
+        const vector<casa::uInt> imageShapeVector = itsParset.getUintVector("Images.shape");
+        const casa::uInt nx = imageShapeVector[0];
+        const casa::uInt ny = imageShapeVector[1];
+        const casa::IPosition cubeShape(4, nx, ny, npol, nchan);
+
+        // Use a tile shape appropriate for plane-by-plane access
+        casa::IPosition tileShape(cubeShape.nelements(), 1);
+        tileShape(0) = 256;
+        tileShape(1) = 256;
+
+        const casa::CoordinateSystem csys = createCoordinateSystem(itsParset, nx, ny, f0, inc);
+
+        ASKAPLOG_INFO_STR(logger, "Creating Cube " << itsFilename <<
+                           " with shape [xsize:" << nx << " ysize:" << ny <<
+                           " npol:" << npol << " nchan:" << nchan <<
+                           "], f0: " << f0.getValue("MHz") << " MHz, finc: " <<
+                           inc.getValue("kHz") << " kHz");
+
+        itsCube->create(itsFilename, cubeShape, csys);
+
+        // default flux units are Jy/pixel. If we set the restoring beam
+        // later on, can set to Jy/beam
+        itsCube->setUnits(itsFilename,"Jy/pixel");
+
+        ASKAPLOG_INFO_STR(logger, "Instantiated Cube Builder by creating cube " << itsFilename);
+
+        return itsCube;
     }
 
 
@@ -146,9 +291,34 @@ namespace askap {
             return -1;
         }
 
+        // sort out the cube ....
+        // get this from the parset?
+        casa::Double baseFrequency = 1E9;
+        casa::Double chanWidth = 1E5;
+        casa::Int nchanCube = 8;
+        // The above are all fake until I find a way to get them.
+
+        casa::Quantity f0(baseFrequency,"Hz");
+    /// The width of a channel. THis does <NOT> take account of the variable width
+    /// of Barycentric channels
+        casa::Quantity freqinc(chanWidth,"Hz");
 
 
-        // This is just to test whether this works at all.
+
+        std::string img_name = "image";
+        std::string psf_name = "psf";
+        std::string residual_name = "residual";
+        std::string weights_name = "weights";
+
+
+        ASKAPLOG_DEBUG_STR(logger,"Configuring Spectral Cube");
+        ASKAPLOG_DEBUG_STR(logger,"nchan: " << nchanCube << " base f0: " << f0.getValue("MHz") << " MHz "
+        << " width: " << freqinc.getValue("MHz"));
+
+        itsImageCube = this->Construct(nchanCube, f0, freqinc,img_name);
+        itsPSFCube = this->Construct(nchanCube, f0, freqinc, psf_name);
+        itsResidualCube = this->Construct(nchanCube, f0, freqinc, residual_name);
+        itsWeightsCube = this->Construct(nchanCube, f0, freqinc, weights_name);
 
 
         return 0;
@@ -163,6 +333,82 @@ namespace askap {
         dlg_app_done(APP_FINISHED);
     }
 
+    casa::CoordinateSystem CubeBuilder::createCoordinateSystem(const LOFAR::ParameterSet& parset,
+                                        const casa::uInt nx,
+                                        const casa::uInt ny,
+                                        const casa::Quantity& f0,
+                                        const casa::Quantity& inc)
+    {
+        casa::CoordinateSystem coordsys;
+        const vector<string> dirVector = parset.getStringVector("Images.direction");
+        const vector<string> cellSizeVector = parset.getStringVector("Images.cellsize");
+
+
+        // Direction Coordinate
+        {
+            casa::Matrix<casa::Double> xform(2, 2);
+            xform = 0.0;
+            xform.diagonal() = 1.0;
+            const casa::Quantum<casa::Double> ra = asQuantity(dirVector.at(0), "deg");
+            const casa::Quantum<casa::Double> dec = asQuantity(dirVector.at(1), "deg");
+            ASKAPLOG_DEBUG_STR(logger, "Direction: " << ra.getValue() << " degrees, "
+                               << dec.getValue() << " degrees");
+
+            const casa::Quantum<casa::Double> xcellsize = asQuantity(cellSizeVector.at(0), "arcsec") * -1.0;
+            const casa::Quantum<casa::Double> ycellsize = asQuantity(cellSizeVector.at(1), "arcsec");
+            ASKAPLOG_DEBUG_STR(logger, "Cellsize: " << xcellsize.getValue()
+                               << " arcsec, " << ycellsize.getValue() << " arcsec");
+
+            casa::MDirection::Types type;
+            casa::MDirection::getType(type, dirVector.at(2));
+            const casa::DirectionCoordinate radec(type, casa::Projection(casa::Projection::SIN),
+                                            ra, dec, xcellsize, ycellsize,
+                                            xform, nx / 2, ny / 2);
+
+            coordsys.addCoordinate(radec);
+        }
+
+        // Stokes Coordinate
+        {
+
+            // To make a StokesCoordinate, need to convert the StokesTypes
+            // into integers explicitly
+            casa::Vector<casa::Int> stokes(itsStokes.size());
+            for(unsigned int i=0;i<stokes.size();i++){
+                stokes[i] = itsStokes[i];
+            }
+            const casa::StokesCoordinate stokescoord(stokes);
+            coordsys.addCoordinate(stokescoord);
+
+        }
+        // Spectral Coordinate
+        {
+            const casa::Double refPix = 0.0;  // is the reference pixel
+            const bool barycentre = parset.getBool("barycentre",false);
+            casa::MFrequency::Types freqRef=casa::MFrequency::TOPO;
+            if (barycentre){
+                freqRef = casa::MFrequency::BARY;
+            }
+            casa::SpectralCoordinate sc(freqRef, f0, inc, refPix);
+
+            // add rest frequency, but only if requested, and only for
+            // image.blah, residual.blah, image.blah.restored
+            if (itsRestFrequency.getValue("Hz") > 0.) {
+                if ((itsFilename.find("image.") != string::npos) ||
+                        (itsFilename.find("residual.") != string::npos)) {
+
+                    if (!sc.setRestFrequency(itsRestFrequency.getValue("Hz"))) {
+                        ASKAPLOG_ERROR_STR(logger, "Could not set the rest frequency to " <<
+                                           itsRestFrequency.getValue("Hz") << "Hz");
+                    }
+                }
+            }
+
+            coordsys.addCoordinate(sc);
+        }
+
+        return coordsys;
+    }
 
 
 } // namespace
--- apps/askap/factory/CubeBuilder.h
+++ apps/askap/factory/CubeBuilder.h
@@ -10,7 +10,7 @@
 
 #include <daliuge/DaliugeApplication.h>
 
-
+#include <imageaccess/ImageAccessFactory.h>
 
 #include <boost/shared_ptr.hpp>
 
@@ -18,7 +18,11 @@
 #include <Common/ParameterSet.h>
 
 
-
+#include <casacore/images/Images/PagedImage.h>
+#include <casacore/lattices/Lattices/PagedArray.h>
+#include <casacore/casa/Arrays/Array.h>
+#include <casacore/coordinates/Coordinates/CoordinateSystem.h>
+#include <casacore/casa/Quanta.h>
 
 
 namespace askap {
@@ -44,6 +48,15 @@ namespace askap {
 
         static DaliugeApplication::ShPtr createDaliugeApplication(dlg_app_info *raw_app);
 
+        boost::shared_ptr<accessors::IImageAccess> Construct(
+                    const casa::uInt nchan,
+                    const casa::Quantity& f0,
+                    const casa::Quantity& inc,
+                    const std::string& name = "");
+
+        boost::shared_ptr<accessors::IImageAccess> Construct(const std:: string& name);
+
+
         virtual int init(const char ***arguments);
 
         virtual int run();
@@ -61,12 +74,27 @@ namespace askap {
             //! @details key value list of configuration options
             LOFAR::ParameterSet itsParset;
 
-          
+            boost::shared_ptr<accessors::IImageAccess> itsImageCube;
+            boost::shared_ptr<accessors::IImageAccess> itsPSFCube;
+            boost::shared_ptr<accessors::IImageAccess> itsResidualCube;
+            boost::shared_ptr<accessors::IImageAccess> itsWeightsCube;
+            boost::shared_ptr<accessors::IImageAccess> itsPSFimageCube;
+            boost::shared_ptr<accessors::IImageAccess> itsRestoredCube;
 
 
+            /// Rest frequency to be written to the cubes
+            casa::Quantum<double> itsRestFrequency;
 
+            std::string itsFilename;
 
+            casa::Vector<casa::Stokes::StokesTypes> itsStokes;
 
+            casa::CoordinateSystem
+            createCoordinateSystem(const LOFAR::ParameterSet& parset,
+                                   const casa::uInt nx,
+                                   const casa::uInt ny,
+                                   const casa::Quantity& f0,
+                                   const casa::Quantity& inc);
 
     };
 
