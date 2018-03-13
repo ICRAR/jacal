2a840b8e30917e04bcd92c7fd39c5a95497cc03b
Chen Wu <chen.wu@icrar.org>
2018-03-13 20:41:02 +0800

    added sender_means_no_dlg and sender_means_no_dlg_binary to plot

--- deploy/bracewell/plot_perf.py
+++ deploy/bracewell/plot_perf.py
@@ -16,6 +16,12 @@ ingest_means = (65.642, 340.8659, 679.328)
 #ingest_std = (3, 5, 2, 3, 3)
 rects2 = ax.bar(ind + width, ingest_means, width, color='y')#, yerr=ingest_std)
 
+sender_means_no_dlg = (70.467, 338.990, 676.056)
+rects4 = ax.bar(ind + width * 3, sender_means_no_dlg, width, color='cyan')#, yerr=ingest_std)
+
+sender_means_no_dlg_binary = (71.215, 338.194, 672.861)
+rects5 = ax.bar(ind + width * 4, sender_means_no_dlg_binary, width, color='g')
+
 data_vol = (21, 101, 200)
 
 ax1 = ax.twinx()
@@ -26,12 +32,14 @@ ax1.set_ylim([0, 300])
 # add some text for labels, title and axes ticks
 ax.set_ylabel('Seconds', fontsize=14)
 ax.set_xlabel('Number of simulated time steps', fontsize=14)
-ax.set_title('Completition time and data volume per node by Senders (5 nodes) and Ingest pipeline (5 nodes)')
-ax.set_xticks(ind + width / 2)
+ax.set_title('Completition time and data volume per node by Senders and Ingest pipeline or MPI Receiver')
+ax.set_xticks(ind + width * 1.5)
 ax.set_xticklabels(('   1200', '   6000', '   12000'))
 ax.set_ylim([0, 800])
 
-ax.legend((rects1[0], rects2[0]), ('Sender', 'Ingest'), loc="upper left")
+ax.legend((rects1[0], rects2[0], rects4[0], rects5[0]), ('Sender (5 nodes)', 'Ingest (5 nodes)',\
+ 'Sender (2 nodes) with MPI receiver. No DALiuGE, No dummy flagging', \
+ 'Sender (1 node) with MPI receiver binary dump'), loc="upper left")
 ax1.legend((rects3[0],), ('Data volume',), loc="upper right")
 
 ax.grid(True, linestyle='-.', which='major', color='lightgrey',
@@ -51,6 +59,8 @@ def autolabel(rects, x_offset=0, y_offset=1.01):
 
 autolabel(rects1, x_offset=-1.5 * width)
 autolabel(rects2, x_offset=0.5 * width)
+autolabel(rects4, x_offset=width)
+autolabel(rects5, x_offset=width * 1.5, y_offset=0.9)
 #autolabel(rects3, x_offset=0.5 * width)
 
 plt.show()
