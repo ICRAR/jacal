e50bce680baa9f7eaaa0d5fbcdcf1f82dd44f2b2
Rodrigo Tobar <rtobar@icrar.org>
2018-06-20 15:04:04 +0800

    run_test.sh in working order again
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- apps/askap/functests/basic/run_test.sh
+++ apps/askap/functests/basic/run_test.sh
@@ -9,29 +9,29 @@ then
 fi
 cd "$(dirname $this)"
 
-JACAL_LIB=../../jacal/apps/askap/libaskapsoft_dlg.so
+JACAL_LIB=../../libaskapsoft_dlg.so
 for f in setup_askap.sh setup_daliuge.sh $JACAL_LIB; do
-    if [ ! -e setup_askap.sh ]; then
+    if [ ! -e ${f} ]; then
+        echo "${f} not found, cannot continue" 1>&2
         exit -1
     fi
 done
 
 source setup_askap.sh
+unset PYTHONPATH
 source setup_daliuge.sh $@
-sleep 5
 cp $JACAL_LIB /tmp/libaskapsoft_dlg.so
-
-if [ -e /tmp/single.in ]; then
-    rm /tmp/single.in
-fi
 cp single.in /tmp
+sleep 5
 
-if [ -e /tmp/single.in ]; then
-    if [ -e run_jacal.sh ]; then
-        source run_jacal.sh
-    else
-#        exit -1
-    fi
-else
+if [ ! -e /tmp/single.in ]; then
     echo "Failed to copy parfile to /tmp"
+	 exit -1
 fi
+
+echo "Starting basic test"
+dlg unroll-and-partition -L ./basic_image.json | dlg map -N localhost,localhost -i 1 | dlg submit -H localhost -p 8001
+sleep 20
+echo "Finishing test now"
+killall dlg
+wait
