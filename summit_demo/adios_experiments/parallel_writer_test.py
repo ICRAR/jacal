cb8dec25ab1e9fd2bc08a7af448a4d0fb2aa3001
Rodrigo Tobar <rtobar@icrar.org>
2018-12-03 12:32:01 +0800

    New example script on how to write with ADIOS2
    This script shows us how to write columns with casacore-enabled ADIOS2 using
    the Adios2StMan storage manager. This runs well both in a single-rank MPI
    communicator, or in multiple-rank communicators. I have only tried it in a
    single node, but there's no reason why it shouldn't work across many nodes.
    
    The script allows users to specify the column type (only a reduced set of
    datatypes for the time being), and whether to use all MPI ranks or a subset of
    them (the even ones) to perform the writing.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

---
+++ summit_demo/adios_experiments/parallel_writer_test.py
@@ -0,0 +1,73 @@
+#
+# ICRAR - International Centre for Radio Astronomy Research
+# (c) UWA - The University of Western Australia, 2018
+# Copyright by UWA (in the framework of the ICRAR)
+#
+# (c) Copyright 2018 CSIRO
+# Australia Telescope National Facility (ATNF)
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# PO Box 76, Epping NSW 1710, Australia
+# atnf-enquiries@csiro.au
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307  USA
+#
+import argparse
+
+from casacore import tables
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output', help='The table to write', default='my_table')
+    parser.add_argument('-t', '--column-type', help='The type of the column to write', default='float')
+    parser.add_argument('-c', '--column-name', help='The name of the column to write', default=None)
+    parser.add_argument('-e', '--even', action='store_true', help='Write only from even ranks (to check sub-communicator writing)', default=False)
+    parser.add_argument('-n', '--nrows', type=int, help='Number of rows to write per rank', default=100)
+    opts = parser.parse_args()
+
+    colname = opts.column_name or 'MY_%s_COLUMN' % opts.column_type.upper()
+    ref_val = None
+    if opts.column_type == 'float':
+        ref_val = 0.
+    elif opts.column_type == 'int':
+        ref_val = 0
+    elif opts.column_type == 'bool':
+        ref_val = False
+    else:
+        parser.error('Only float, int and bool are supported column type names')
+
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+
+    if opts.even:
+        size = (size + 1) // 2
+
+    scd = tables.makescacoldesc(colname, ref_val, datamanagertype='Adios2StMan')
+    td = tables.maketabdesc([scd])
+    t = tables.table(opts.output, td, nrow=opts.nrows * size)
+    with t:
+        if opts.even and rank % 2 != 0:
+            return
+        tc = tables.tablecolumn(t, colname)
+        start = opts.nrows * rank
+        if opts.even:
+            start //= 2
+        tc[start:start + opts.nrows] = range(opts.nrows)
+
+if __name__ == '__main__':
+    main()
