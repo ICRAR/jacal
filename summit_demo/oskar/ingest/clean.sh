7af8ae95a058c56a802477cb94255e022b0ab71d
davepallot <dave.pallot@uwa.edu.au>
2018-12-21 09:42:29 +0800

    Added comments to clean.sh

--- summit_demo/oskar/ingest/clean.sh
+++ summit_demo/oskar/ingest/clean.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
+# Create an image and then deconvolve it.
+# This task will work for single-field data, or for multi-field mosaics
+# in both narrow and wide-field imaging modes.
+
 function clean() {
+    # niter      (1): Maximum number of clean iterations.
+    # imsize_x   (2): x image size in pixels.
+    # imsize_y   (3): y image size in pixels.
+    # cell_x     (4): x cell size. default unit arcsec.
+    # cell_y     (5): y cell size. default unit arcsec.
+    # image_name (6): name of output images.
+    # vis     (7..n): visibility input file(s).
+
     if (( "$#" < 7 )); then
         return 1;
     fi
