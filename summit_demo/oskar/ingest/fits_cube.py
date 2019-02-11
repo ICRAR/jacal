e3aadc96e135d45e8af63b97dbcb7afbab89e1ff
dpallot <dave.pallot@uwa.edu.au>
2019-02-11 12:09:42 +0800

    Check concatenated cubes share the same dimension. Modify the delta freq to be the diff between the start freq of 2 images.

--- summit_demo/oskar/ingest/fits_cube.py
+++ summit_demo/oskar/ingest/fits_cube.py
@@ -6,15 +6,28 @@ from astropy.io import fits
 from shutil import copyfile
 
 
-def image_size_and_num_channels(fits_file):
+def eformat(f, prec, exp_digits):
+    s = "%.*E"%(prec, f)
+    mantissa, exp = s.split('E')
+    # add 1 to digits as 1 is taken by sign +/-
+    return "%sE%+0*d"%(mantissa, exp_digits+1, int(exp))
+
+
+def get_header(fits_file):
     header = fits.getheader(fits_file, 0)
     if header['NAXIS'] > 3:
         raise Exception('Invalid number of axis')
     if header['CTYPE3'] != 'FREQ':
         raise Exception('3rd axis is not FREQ')
+    return header
 
-    image_size = int(header['NAXIS1'] * header['NAXIS2'] * math.fabs(header['BITPIX'])/8)
-    return image_size, int(header['NAXIS3'])
+
+def get_image_size_bytes(header):
+    return int(header['NAXIS1'] * header['NAXIS2'] * math.fabs(header['BITPIX']) / 8)
+
+
+def get_image_dimension(header):
+    return header['NAXIS1'], header['NAXIS2'], header['NAXIS3'], header['BITPIX']
 
 
 def header_size_obj(fits_input_obj):
@@ -39,9 +52,12 @@ def modify_header(fits_file, key, value):
         val = "'{0}'".format(str(value))
         entry = "{0}  = {1}".format(key, val)
     else:
-        val = value
+        if isinstance(value, float):
+            val = eformat(value, 12, 2)
+        else:
+            val = value
         white_space = [' ']*(21 - len(str(val)))
-        entry = "{0}  ={1}{2}".format(key, ''.join(white_space), value)
+        entry = "{0}  ={1}{2}".format(key, ''.join(white_space), val)
 
     row_bytes = bytes(entry.encode('ascii')) + bytes([0x20]*(80-len(entry)))
     read_bytes = 0
@@ -51,7 +67,7 @@ def modify_header(fits_file, key, value):
             row = fits_input_obj.read(80).decode('ascii')
 
             if 'END' in row.upper():
-                break
+                raise Exception('{0} not found'.format(key))
 
             if row.startswith(key):
                 fits_input_obj.seek(read_bytes)
@@ -74,17 +90,30 @@ def concat_images(fits_cube_output, fits_image_input):
         copyfile(first_input, fits_cube_output)
 
     hdr_size = header_size(fits_cube_output)
-    image_size, num_channels = image_size_and_num_channels(fits_cube_output)
+    hdr = get_header(fits_cube_output)
+    orig_x, orig_y, orig_num_chan, orig_bit = get_image_dimension(hdr)
+    image_size = get_image_size_bytes(hdr)
 
-    total_size = hdr_size + (image_size * num_channels)
-    total_channels = num_channels
+    total_size = hdr_size + (image_size * orig_num_chan)
+    total_channels = orig_num_chan
+
+    delta_freq = None
 
     with open(fits_cube_output, 'rb+') as f:
         f.seek(total_size)
 
         # loop through input and append images to original cube
         for input_fits in input_iter:
-            in_image_size, in_num_channels = image_size_and_num_channels(input_fits)
+            in_hdr = get_header(input_fits)
+            in_x, in_y, in_num_channels, in_bit = get_image_dimension(in_hdr)
+            in_image_size = get_image_size_bytes(in_hdr)
+
+            if in_image_size != image_size:
+                raise Exception("image dimensions do not match")
+
+            if delta_freq is None:
+                delta_freq = float(in_hdr['CRVAL3']) - float(hdr['CRVAL3'])
+
             total_channels += in_num_channels
 
             with open(input_fits, 'rb') as inf:
@@ -107,3 +136,5 @@ def concat_images(fits_cube_output, fits_image_input):
 
     # update the total number of freq channels in final image cube
     modify_header(fits_cube_output, 'NAXIS3', total_channels)
+    if delta_freq:
+        modify_header(fits_cube_output, 'CDELT3', delta_freq)
