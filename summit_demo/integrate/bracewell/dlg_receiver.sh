c165615a4d7d74d43efeb2e31ece58c866e9a975
Rodrigo Tobar <rtobar@icrar.org>
2019-02-13 17:40:45 +1100

    New -a option to specify directory with apps
    SLURM will execute the script most probably from a directory under
    /var/lib/... so the calculation of $this_dir in the scripts submitted to
    and executed by SLURM doesn't necessarily reflect the actual origin of
    the source code.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- summit_demo/integrate/bracewell/dlg_receiver.sh
+++ summit_demo/integrate/bracewell/dlg_receiver.sh
@@ -7,7 +7,7 @@ this_dir=`eval "cd \$(dirname $0); echo \$PWD; cd \$OLDPWD"`
 load_common
 
 # Make sure our node managers see our drops
-export PYTHONPATH="$this_dir/../../oskar/ingest:$PYTHONPATH"
+export PYTHONPATH="${apps_rootdir}:$PYTHONPATH"
 cd "$outdir"
 mpirun python -m dlg.deploy.pawsey.start_dfms_cluster \
     -l . -L $this_dir/bracewell_mvp.json --part-algo mysarkar
