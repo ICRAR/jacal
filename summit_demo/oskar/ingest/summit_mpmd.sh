da33dd48acd89790b2da4be3195fa513c9e27e4d
Jason Wang <jason.wang@icrar.org>
2019-02-14 22:23:01 -0500

    for debuging Summit

--- summit_demo/oskar/ingest/summit_mpmd.sh
+++ summit_demo/oskar/ingest/summit_mpmd.sh
@@ -9,22 +9,18 @@
 #BSUB -J rialto2-142
 #BSUB -W 00:05
 #BSUB -nnodes 1
-#BSUB -o /gpfs/alpine/csc303/scratch/wangj/data/log/rialto2-142-%J-%I.log
-#BSUB -e /gpfs/alpine/csc303/scratch/wangj/data/log/rialto2-142-%J-%I.err
 
+##BSUB -o /gpfs/alpine/csc303/scratch/wangj/data/log/rialto2-142-%J-%I.log
+##BSUB -e /gpfs/alpine/csc303/scratch/wangj/data/log/rialto2-142-%J-%I.err
 
 # scenario: AA4 telescope config, 1 channel per GPU with 6 GPUs running in parallel
 SCENARIO="rialto2-142"
-GPU_COUNT=6
+GPU_COUNT=2
 USE_GPUS="true"
 let "uidx = $GPU_COUNT - 1"
 
 # push some info to stdout showing that the job has started
 echo "LSB_NODENAME: <`hostname`>"
-echo "LSB_ARRAY_TASK_ID: <${LSB_JOBINDEX}>"
-echo "CUDA_VISIBLE_DEVICES: <${CUDA_VISIBLE_DEVICES}>"
-#echo "all SLURM_* variables:"
-#env | grep -i slurm[_d]
 
 idx=$LSB_JOBINDEX
 
@@ -62,14 +58,15 @@ PHASE_CENTRE_RA_DEG="201"
 PHASE_CENTRE_DEC_DEG="-44"
 MAX_SOURCES_PER_CHUNK=50000
 SKY_DIR=${SKY_DIR}/EOR
-SM_NAME=("sky_eor_model_f210.12" "sky_eor_model_f211.13" "sky_eor_model_f212.16" "sky_eor_model_f213.19" "sky_eor_model_f214.24" "sky_eor_model_f215.03")
+#SM_NAME=("sky_eor_model_f210.12" "sky_eor_model_f211.13" "sky_eor_model_f212.16" "sky_eor_model_f213.19" "sky_eor_model_f214.24" "sky_eor_model_f215.03")
+SM_NAME=("sky_eor_model_f210.12" "sky_eor_model_f210.12" "sky_eor_model_f212.16" "sky_eor_model_f213.19" "sky_eor_model_f214.24" "sky_eor_model_f215.03")
 SM_FILE=()
 for fidx in "${SM_NAME[@]}"; do SM_FILE+=("${SKY_DIR}/${fidx}.osm"); done
 
 cd $APP_ROOT
 
-rm summit_simulator_erf
-rm summit_imager_erf
+rm -f summit_simulator_erf
+rm -f summit_imager_erf
 
 for idx in $(seq 0 $uidx) ; do
 
@@ -92,7 +89,7 @@ for idx in $(seq 0 $uidx) ; do
 
     # make max samples a multiple of the GPUs; 12 is a multiple of 4 (Bracewell) as well as 6 (Summit)
     oskar_sim_interferometer --set "${INTER_INI}.${idx}" interferometer/max_time_samples_per_block 12
-    oskar_sim_interferometer --set "${INTER_INI}.${idx}" interferometer/oskar_vis_filename "${VISNAME}.${idx}"
+    oskar_sim_interferometer --set "${INTER_INI}.${idx}" interferometer/oskar_vis_filename ${VISNAME}.${idx}
     oskar_sim_interferometer --set "${INTER_INI}.${idx}" simulator/cuda_device_ids $idx
     oskar_sim_interferometer --set "${INTER_INI}.${idx}" observation/start_frequency_hz ${START_FREQUENCY_HZ[$idx]}
     oskar_sim_interferometer --set "${INTER_INI}.${idx}" observation/frequency_inc_hz ${FREQUENCY_INC_HZ}
@@ -104,19 +101,35 @@ for idx in $(seq 0 $uidx) ; do
     oskar_imager --set "${IMAGER_INI}.${idx}" image/fov_deg $FOV_DEG
     oskar_imager --set "${IMAGER_INI}.${idx}" image/size $IMG_SIZE
     oskar_imager --set "${IMAGER_INI}.${idx}" image/channel_snapshots $CHANNEL_SNAPSHOTS
-    oskar_imager --set "${IMAGER_INI}.${idx}" image/input_vis_data "${VISNAME}.${idx}"
-    oskar_imager --set "${IMAGER_INI}.${idx}" image/root_path "$FITSROOT.${idx}"
+    oskar_imager --set "${IMAGER_INI}.${idx}" image/input_vis_data ${VISNAME}.${idx}
+    oskar_imager --set "${IMAGER_INI}.${idx}" image/root_path $FITSROOT.${idx}
 
-    echo "app ${idx}: oskar_sim_interferometer \"${INTER_INI}.${idx}\"" >> summit_simulator_erf
-    echo "app ${idx}: oskar_imager \"${IMAGER_INI}.${idx}\"" >> summit_imager_erf
+    echo "app ${idx}: oskar_sim_interferometer ${INTER_INI}.${idx}" >> summit_simulator_erf
+    echo "app ${idx}: oskar_imager ${IMAGER_INI}.${idx}" >> summit_imager_erf
 done
+
+
+
+
+
+echo "overlapping-rs: warn" >> summit_simulator_erf
+echo "overlapping-rs: warn" >> summit_imager_erf
+
+echo "oversubscribe_cpu: warn" >> summit_simulator_erf
+echo "oversubscribe_cpu: warn" >> summit_imager_erf
+
+echo "oversubscribe_mem: allow" >> summit_simulator_erf
+echo "oversubscribe_mem: allow" >> summit_imager_erf
+
+echo "oversubscribe_gpu: allow" >> summit_simulator_erf
+echo "oversubscribe_gpu: allow" >> summit_imager_erf
 
 echo "launch_distribution : packed" >> summit_simulator_erf
 echo "launch_distribution : packed" >> summit_imager_erf
 
 for idx in $(seq 0 $uidx) ; do
-    echo "1 : {host: 1; cpu: {${idx}}; gpu :{${idx}} } : app ${idx}" >> summit_simulator_erf
-    echo "1 : {host: 1; cpu: {${idx}}; gpu :{${idx}} } : app ${idx}" >> summit_imager_erf
+    echo "rank: ${idx} : {host: 1; cpu: {${idx}}; gpu :{${idx}} } : app ${idx}" >> summit_simulator_erf
+    echo "rank: ${idx} : {host: 1; cpu: {${idx}}; gpu :{${idx}} } : app ${idx}" >> summit_imager_erf
 done
 
 # run simulator
