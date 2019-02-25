d811fe412550b3bc3bf383e057cc7e3b460edefc
Rodrigo Tobar <rtobar@icrar.org>
2019-02-25 16:37:36 +1100

    More flexible get_waiting_time script
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- summit_demo/integrate/bracewell/get_waiting_time.sh
+++ summit_demo/integrate/bracewell/get_waiting_time.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-	echo "Usage: $0 <jobID>"
+	echo "Usage: $0 <jobID1> ..."
 	exit 1
 fi
 
-times=`sacct -n -X -j $1 --format Submit,Start --parsable2`
-submit=`echo $times | cut -f1 -d'|'`
-start=`echo $times | cut -f2 -d'|'`
-diff=$((`date -d $start +%s` - `date -d $submit +%s`))
-echo "Waiting time was $diff [s]"
+for job_id in $@; do
+	times=`sacct -n -X -j $job_id --format Submit,Start --parsable2`
+	submit=`echo $times | cut -f1 -d'|'`
+	start=`echo $times | cut -f2 -d'|'`
+	diff=$((`date -d $start +%s` - `date -d $submit +%s`))
+	echo "Waiting time for job $job_id was $diff [s]"
+done
