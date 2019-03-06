9b0140abbc5898e8754ae2c18d5353644b5fd3ad
Rodrigo Tobar <rtobar@icrar.org>
2019-03-06 20:45:01 +1100

    Adding binding reporting, using --hetero-nodes
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- summit_demo/integrate/bracewell/dlg_receiver.sh
+++ summit_demo/integrate/bracewell/dlg_receiver.sh
@@ -9,7 +9,8 @@ load_common
 # Make sure our node managers see our drops
 export PYTHONPATH="${apps_rootdir}:$PYTHONPATH"
 cd "$outdir"
-mpirun python -m dlg.deploy.pawsey.start_dfms_cluster \
+mpirun --report-bindings --bind-to core --hetero-nodes \
+    python -m dlg.deploy.pawsey.start_dfms_cluster \
     -l . \
     -L $logical_graph \
     --part-algo mysarkar \
--- summit_demo/integrate/bracewell/mpi_oskar_sender.sh
+++ summit_demo/integrate/bracewell/mpi_oskar_sender.sh
@@ -10,6 +10,6 @@ APP_ROOT="$this_dir/../../oskar/ingest"
 export PYTHONPATH="$APP_ROOT:$PYTHONPATH"
 cd "$APP_ROOT"
 
-echo "Sleeping for 10 seconds to make sure the AWS queue is valid"
-sleep 10
-mpirun python "$APP_ROOT/bw_send.py" --conf $APP_ROOT"/conf/send%02d.json"
+echo "Sleeping for 15 seconds to make sure the AWS queue is valid"
+sleep 15
+mpirun --report-bindings --hetero-nodes python "$APP_ROOT/bw_send.py" --conf $APP_ROOT"/conf/send%02d.json"
