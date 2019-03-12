30c536b37465ec03d9d29966e6a8e95e6cf4540d
Rodrigo Tobar <rtobar@icrar.org>
2019-03-12 16:45:17 +1100

    Simpler sender scripting
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- summit_demo/integrate/bracewell/mpi_oskar_sender.sh
+++ summit_demo/integrate/bracewell/mpi_oskar_sender.sh
@@ -7,9 +7,8 @@ this_dir=`eval "cd \$(dirname $0); echo \$PWD; cd \$OLDPWD"`
 load_common
 
 APP_ROOT="$this_dir/../../oskar/ingest"
-export PYTHONPATH="$APP_ROOT:$PYTHONPATH"
 cd "$APP_ROOT"
 
 echo "Sleeping for 15 seconds to make sure the AWS queue is valid"
 sleep 15
-mpirun --report-bindings --hetero-nodes python "$APP_ROOT/bw_send.py" --conf $APP_ROOT"/conf/send%02d.json"
+mpirun --report-bindings --hetero-nodes python bw_send.py --conf conf/send%02d.json
