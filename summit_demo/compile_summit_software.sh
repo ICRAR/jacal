b2706926feaaffd052d8ddd2fd1d90cb6057dbc6
rtobar <rtobar@icrar.org>
2019-03-21 09:01:26 +0800

    More flexible cmake
    The cmake binary can now be specified on the command line, defaults to cmake, and is only automatically changed to cmake3 when the system is CentOS.

--- summit_demo/compile_summit_software.sh
+++ summit_demo/compile_summit_software.sh
@@ -32,6 +32,7 @@ print_usage() {
 	echo "Options:"
 	echo " -s <system>   Target system, supported values are centos (default) and ubuntu"
 	echo " -c <compiler> Compiler suite to use, supported values are gcc (default) and clang"
+	echo " -m <cmake>    Cmake binary to use, must be >= 3.1, defaults to cmake"
 	echo " -j <jobs>     Number of parallel compilation jobs, defaults to 1"
 	echo " -p <prefix>   Prefix for installation, defaults to /usr/local"
 	echo " -o            Do *not* build OSKAR"
@@ -69,6 +70,7 @@ check_supported_values() {
 
 system=centos
 compiler=gcc
+cmake=cmake
 jobs=1
 prefix=/usr/local
 build_oskar=yes
@@ -80,7 +82,7 @@ casarest_opts=
 yandasoft_opts=
 oskar_opts=
 
-while getopts "h?s:c:j:p:oiaC:A:r:y:O:" opt
+while getopts "h?s:c:m:j:p:oiaC:A:r:y:O:" opt
 do
 	case "$opt" in
 		[h?])
@@ -92,6 +94,9 @@ do
 			;;
 		c)
 			compiler="$OPTARG"
+			;;
+		m)
+			cmake="$OPTARG"
 			;;
 		j)
 			jobs="$OPTARG"
@@ -142,9 +147,7 @@ else
 fi
 
 if [ $system == centos ]; then
-	CMAKE=cmake3
-else
-	CMAKE=cmake
+	cmake=cmake3
 fi
 
 install_dependencies() {
@@ -228,7 +231,7 @@ build_and_install() {
 	else
 		comp_opts="-DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc"
 	fi
-	try ${CMAKE} .. -DCMAKE_INSTALL_PREFIX="$prefix" $comp_opts "$@"
+	try ${cmake} .. -DCMAKE_INSTALL_PREFIX="$prefix" $comp_opts "$@"
 	try make all -j${jobs}
 	try make install -j${jobs}
 	cd ../..
