438c5565ee2cc4cbe4291f0424b9c82b5a0d5144
Rodrigo Tobar <rtobar@icrar.org>
2019-04-12 05:03:03 +0000

    Module list for pleiades
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- summit_demo/integrate/common.sh
+++ summit_demo/integrate/common.sh
@@ -1,12 +1,21 @@
 #/bin/bash
 
 function load_modules {
-	module use /flush1/tob020/modulefiles
-	module load yandasoft/default
-	module load oskar/2.7.1-adios
-	module load spead2/1.10.0
-	module load casacore/3.0.0-adios
-	module load adios/2.2.0
+
+	if [ "$SLURM_CLUSTER_NAME" = pleiades ]; then
+		module use /scratch/summit_demo/modulefiles
+		module load summit_demo
+		module load openmpi/default
+	else
+		# Bracewell; put inside an "elif" if we need
+		# to add more environments
+		module use /flush1/tob020/modulefiles
+		module load yandasoft/default
+		module load oskar/2.7.1-adios
+		module load spead2/1.10.0
+		module load casacore/3.0.0-adios
+		module load adios/2.2.0
+	fi
 
 	if [ -n "$venv" ]; then
 		source $venv/bin/activate
