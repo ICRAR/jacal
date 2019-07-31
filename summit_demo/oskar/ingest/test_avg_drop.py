ead143746c1b9f0b305562cb83c29124aec532b8
Rodrigo Tobar <rtobar@icrar.org>
2019-07-31 16:01:03 +0800

    Easier usage of test_avg_drop plus cleanup
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- summit_demo/oskar/ingest/test_avg_drop.py
+++ summit_demo/oskar/ingest/test_avg_drop.py
@@ -1,13 +1,11 @@
 import os
 import sys
-import time
 import logging
 import unittest
 
 from dlg import droputils
-from dlg.ddap_protocol import AppDROPStates
 from dlg.drop import InMemoryDROP, FileDROP
-from avg_drop import AveragerSinkDrop, AveragerRelayDrop
+from avg_drop import AveragerSinkDrop
 from signal_drop import SignalGenerateAndAverageDrop
 
 
@@ -36,10 +34,10 @@ class TestAverager(unittest.TestCase):
                                               start_freq=210200000,
                                               freq_step=4000,
                                               use_gpus=int(os.environ.get('USE_GPUS', 0)),
-                                              num_freq_steps=int(os.environ.get('NUM_CHANNELS', 3)),
+                                              num_freq_steps=int(os.environ.get('NUM_CHANNELS', 1)),
                                               telescope_model_path='./conf/%s.tm' % tm,
                                               sky_model_file_path="./conf/eor_model_list.csv",
-                                              num_time_step=int(os.environ.get('NUM_TIME_STEPS', 5)))
+                                              num_time_step=int(os.environ.get('NUM_TIME_STEPS', 1)))
 
         sink = AveragerSinkDrop('2', '2',
                                 stream_listen_port_start=stream_port,
@@ -54,3 +52,6 @@ class TestAverager(unittest.TestCase):
 
         with droputils.DROPWaiterCtx(self, ms, 1000):
             signal.async_execute()
+
+if __name__ == '__main__':
+    unittest.main()
