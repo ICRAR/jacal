e48b88f780d1bb3c71298f69d8cc4dc137c6ea34
Jason Wang <jason.wang@icrar.org>
2019-08-04 01:34:16 -0400

    added some log information for multi-process cimager run

--- summit_demo/integrate/run_image_graph_nodlg.sh
+++ summit_demo/integrate/run_image_graph_nodlg.sh
@@ -10,9 +10,11 @@ total_processes="$4"
 banner "Arguments passed from job script"
 echo "venv = $venv"
 echo "outdir = $outdir"
+echo "processes_per_node = $processes_per_node"
+echo "total_processes = $total_processes"
 echo "**************************************"
-
 load_modules
+echo "**************************************"
 
 env > $outdir/env.txt
 git rev-parse HEAD > $outdir/commit.txt
@@ -21,6 +23,7 @@ starting_process_id=$(( ($subjob_id - 1) * ($processes_per_node) + 1 ))
 ending_process_id=$(( $subjob_id * $processes_per_node ))
 total_processes=$(( $total_processes + 1 ))
 
+banner "Processing visibility data"
 for r in `seq $starting_process_id $ending_process_id`
 do
     if [ $total_processes -gt $r ]; then
@@ -30,3 +33,4 @@ do
         cd "../.."
     fi
 done
+echo "**************************************"
