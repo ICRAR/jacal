a1d90b71efc1f56ed0a7d8540d203ca43242b32b
Kevin Vinsen <kvn1961@gmail.com>
2019-11-24 11:42:37 +0800

    SHAO docker scripts

--- shao_demo/scripts/shao/start_dim_docker.sh
+++ shao_demo/scripts/shao/start_dim_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d -it --name daliuge_dim --mount type=bind,source=/ssd/ASKAP/jacal,target=/ssd/ASKAP/jacal \
+docker run -it --name daliuge_dim --mount type=bind,source=/ssd/ASKAP/jacal,target=/ssd/ASKAP/jacal \
     --mount type=bind,source=/home/kvinsen,target=/user \
     -p 8001:8001 -p 5555:5555 -p 6666:6666 \
     192.168.6.123:5000/jacal/jacal-run  \
