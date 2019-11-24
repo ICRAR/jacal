2a93272998dc5de00327b68624f78a47030b84e1
Kevin Vinsen <kvn1961@gmail.com>
2019-11-24 09:24:25 +0800

    mssplit on big file

--- shao_demo/scripts/shao/mssplit.sh
+++ shao_demo/scripts/shao/mssplit.sh
@@ -2,7 +2,7 @@
 
 # Use mssplit to split the file
 docker run -it --rm  --name mssplit --mount type=bind,source=/o9000/ASKAP/dingo-data/G12_test,target=/data_in \
-    --mount type=bind,source=/ssd/ASKAP/dingo-data,target=/data_out \
+    --mount type=bind,source=/ssd/ASKAP/dingo-data1,target=/data_out \
     --mount type=bind,source=/ssd/ASKAP/jacal/shao_demo/scripts/shao,target=/run_dir \
     192.168.6.123:5000/jacal/jacal-run \
     \
