65c09fca93ff5dd4a5b9fb3e1d21706efaabc15f
Kevin Vinsen <kvn1961@gmail.com>
2019-11-24 11:12:36 +0800

    SHAO docker scripts

--- shao_demo/scripts/shao/start_dim_docker.sh
+++ shao_demo/scripts/shao/start_dim_docker.sh
@@ -6,5 +6,5 @@ docker run -d -it --name daliuge_dim --mount type=bind,source=/ssd/ASKAP/jacal,t
     192.168.6.123:5000/jacal/jacal-run  \
     \
     dlg dim -vvv -H 0.0.0.0 --ssh-pkey-path /user/.ssh/id_daliuge \
-    --nodes 192.168.6.35,192.168.6.36,192.168.6.37,192.168.6.38,192.168.6.39,192.168.6.40,192.168.6.41,192.168.6.42 \
+    --nodes 192.168.6.36,192.168.6.37,192.168.6.38,192.168.6.39,192.168.6.40,192.168.6.41,192.168.6.42 \
     --log-dir "/user/daliuge-dim-$(hostname -I | grep -Po '192\.168\.6\.[0-9]+')" --max-request-size 10
