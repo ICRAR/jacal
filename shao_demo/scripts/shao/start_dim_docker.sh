fb3fff4a856adeecb08d30061cf6d298b8eb97e0
Kevin Vinsen <kvn1961@gmail.com>
2019-11-24 11:02:50 +0800

    SHAO docker scripts

--- shao_demo/scripts/shao/start_dim_docker.sh
+++ shao_demo/scripts/shao/start_dim_docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 docker run -d -it --name daliuge_dim --mount type=bind,source=/ssd/ASKAP/jacal,target=/ssd/ASKAP/jacal \
-    --mount type=bind,source=/home/kvinsen,target=/home/user \
+    --mount type=bind,source=/home/kvinsen,target=/user \
     -p 8001:8001 -p 5555:5555 -p 6666:6666 \
     192.168.6.123:5000/jacal/jacal-run  \
     \
-    dlg dim -vvv -H 0.0.0.0 --ssh-pkey-path /home/user/.ssh/id_daliuge \
+    dlg dim -vvv -H 0.0.0.0 --ssh-pkey-path /user/.ssh/id_daliuge \
     --nodes 192.168.6.35,192.168.6.36,192.168.6.37,192.168.6.38,192.168.6.39,192.168.6.40,192.168.6.41,192.168.6.42 \
-    --log-dir "/home/user/daliuge-dim-$(hostname -I | grep -Po '192\.168\.6\.[0-9]+')" --max-request-size 10
+    --log-dir "/user/daliuge-dim-$(hostname -I | grep -Po '192\.168\.6\.[0-9]+')" --max-request-size 10
--- shao_demo/scripts/shao/start_nms_docker.sh
+++ shao_demo/scripts/shao/start_nms_docker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-docker run -d -it --name daliuge_dim --mount type=bind,source=/ssd/ASKAP/jacal,target=/ssd/ASKAP/jacal \
-    --mount type=bind,source=/home/kvinsen,target=/home/user \
+docker run -d -it --name daliuge_dim --mount type=bind,source=/ssd/ASKAP/jacal,target=/jacal \
+    --mount type=bind,source=/home/kvinsen,target=/user \
+    --mount type=bind,source=/ssd/ASKAP/dingo_data,target=/data \
     -p 8000:8000 -p 5555:5555 -p 6666:6666 \
     192.168.6.123:5000/jacal/jacal-run  \
     \
-    dlg nm -vvv -H 0.0.0.0 --log-dir "/home/user/daliuge-nm-$(hostname -I | grep -Po '192\.168\.6\.[0-9]+')" \
-    --max-request-size 10
+    dlg nm -vvv -H 0.0.0.0 --log-dir "/user/daliuge-nm-$(hostname -I | grep -Po '192\.168\.6\.[0-9]+')" \
+    --max-request-size 10 --dlg-path=/jacal/shao_demo/daliuge
