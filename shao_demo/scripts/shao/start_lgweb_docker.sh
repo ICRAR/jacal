0c174b8944a13a6d7967814cd44584624f13a1c7
Kevin Vinsen <kvn1961@gmail.com>
2019-11-24 11:37:35 +0800

    SHAO docker scripts

--- shao_demo/scripts/shao/start_lgweb_docker.sh
+++ shao_demo/scripts/shao/start_lgweb_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d -it --name daliuge --mount type=bind,source=/ssd/ASKAP/jacal,target=/ssd/ASKAP/jacal \
+docker run -it --name lgweb --mount type=bind,source=/ssd/ASKAP/jacal,target=/ssd/ASKAP/jacal \
     --mount type=bind,source=/home/kvinsen,target=/user \
     -p 8084:8084 \
     192.168.6.123:5000/jacal/jacal-run  \
