c96870c267f23c80dbb46145548996b64b1bc04e
Kevin Vinsen <kvn1961@gmail.com>
2019-11-27 10:52:53 +0800

    cd to where I want the output

--- shao_demo/scripts/localhost/start_nm_docker_it.sh
+++ shao_demo/scripts/localhost/start_nm_docker_it.sh
@@ -4,4 +4,4 @@ docker run -it --rm  --name daliuge_nms --mount type=bind,source=/home/kevin/Wor
     -p 8000:8000 -p 5555:5555 -p 6666:6666 \
     jacal-006-build-jacal \
     \
-    dlg nm -vvv --dlg-path=/tmp -H 0.0.0.0 --log-dir /tmp --dlg-path=/home/kevin/Work/jacal/shao_demo/daliuge
+    /bin/bash -c 'cd /home/kevin/Work/jacal/shao_demo/data && dlg nm -vvv --dlg-path=/tmp -H 0.0.0.0 --log-dir /tmp --dlg-path=/home/kevin/Work/jacal/shao_demo/daliuge'
--- shao_demo/scripts/shao/start_nm_docker.sh
+++ shao_demo/scripts/shao/start_nm_docker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+log_dir="/user/daliuge-nm-$(hostname -I | grep -Po '192\.168\.6\.[0-9]+')"
+
 docker run -d -it --name daliuge_nm --mount type=bind,source=/ssd/ASKAP/jacal,target=/jacal \
     --mount type=bind,source=/home/kvinsen,target=/user \
     --mount type=bind,source=/ssd/ASKAP/dingo-data,target=/data \
     -p 8000:8000 -p 5555:5555 -p 6666:6666 \
     192.168.6.123:5000/jacal/jacal-run  \
     \
-    dlg nm -vvv -H 0.0.0.0 --log-dir "/user/daliuge-nm-$(hostname -I | grep -Po '192\.168\.6\.[0-9]+')" \
-    --max-request-size 10 --dlg-path=/jacal/shao_demo/daliuge
+    /bin/bash -c "cd /data && dlg nm -vvv -H 0.0.0.0 --log-dir $log_dir --max-request-size 10 --dlg-path=/jacal/shao_demo/daliuge"
