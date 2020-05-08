dd96105e9709c5fa02175976b1705c3ef6c166ef
Rowan Worth <rowanw@dugeo.com>
2020-05-08 16:55:25 +0800

    summit: Fix max_sources_per_chunk plumbing

--- summit_demo/integrate/submit_ingest_graph.sh
+++ summit_demo/integrate/submit_ingest_graph.sh
@@ -209,7 +209,7 @@ s/\"use_gpus=.*\"/\"use_gpus=$use_gpus\"/
 # Number of time steps to simulate
 s%\"num_time_steps=.*\"%\"num_time_steps=$time_steps\"%
 
-# Number of time steps to simulate
+# Maximum number of sources to simulate in a single work packet
 s%\"max_sources_per_chunk=.*\"%\"max_sources_per_chunk=$max_spc\"%
 
 # The base port used for internal spead2 communications
